43120df86a768fc6f824071687ce41e66d515f4b
merxentaerp-tech <merxenta.erp@gmail.com>
2026-04-29 10:49:21 +0530

    Update Exchange.js

--- routes/Exchange.js
+++ routes/Exchange.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { createExchange , getExchangeDashboard,getInvoiceForExchange} from "../controller/exchangeController.js";
+import {
+  createExchange,
+  getExchangeDashboard,
+  getInvoiceForExchange
+} from "../controller/exchangeController.js";
+
+import { auth } from "../middlewares/authMiddleware.js"; 
 
 const router = express.Router();
 
 // Create Exchange
-router.get("/invoice/:invoice_number", getInvoiceForExchange);
-router.post("/create", createExchange);
-router.get("/dashboard", getExchangeDashboard);
+router.get("/invoice/:invoice_number", auth, getInvoiceForExchange); 
+router.post("/create", auth, createExchange); 
+router.get("/dashboard", auth, getExchangeDashboard); 
 
 export default router;

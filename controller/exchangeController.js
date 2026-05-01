cc1f0c095a527b6113918cbc6539898bedc44795
merxentaerp-tech <merxenta.erp@gmail.com>
2026-05-01 10:13:05 +0530

    Add files via upload

--- controller/exchangeController.js
+++ controller/exchangeController.js
@@ -1,7 +1,7 @@
 import sequelize from "../config/db.js";
 import { QueryTypes } from "sequelize";
-import SystemActivity from "../model/SystemActivity.js";
-import ActivityLog from "../model/ActivityLog.js";
+import SystemActivity from "../model/systemActivity.js";
+import ActivityLog from "../model/activityLog.js";
 
 // ==============================
 //  GET INVOICE FOR EXCHANGE

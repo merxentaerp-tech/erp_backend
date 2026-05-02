c2b4e9ec5a649f05cc249b8813903ea68cc74cd4
merxentaerp-tech <merxenta.erp@gmail.com>
2026-05-02 16:13:59 +0530

    Update dashboardRoutes.js

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -2,8 +2,8 @@ import express from "express";
 import { getDashboardSummary,getAllReports } from "../controller/dashboardController.js";
 import { auth } from "../middlewares/authMiddleware.js";
 import { getDistrictDashboard,addDistrictItemWithStock} from "../controller/districtController.js"
-import {getFullDashboard ,getHeadOfficeDashboard} from "../controller/headoffice/dashboardController.js";
-
+import {getFullDashboard } from "../controller/headoffice/dashboardController.js";
+import { getHeadOfficeReports } from "../controller/headoffice/headReportsController.js"
 const router = express.Router();
 
 router.get("/summary", auth, getDashboardSummary);
@@ -11,9 +11,8 @@ router.get('/Dis/dash',auth,getDistrictDashboard)
 router.post("/district/item-stock/add", auth, addDistrictItemWithStock);
 router.get('/report',auth,getAllReports)
 
+router.get("/reports",auth, getHeadOfficeReports);
 
-
-router.get("/reports",auth, getHeadOfficeDashboard);
 
 
 router.get("/dashboard/full", getFullDashboard);

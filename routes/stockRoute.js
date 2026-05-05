54426860f547826e2d30f4ef0d79bfd6c63b9ea2
merxentaerp-tech <merxenta.erp@gmail.com>
2026-05-05 12:13:20 +0530

    Update stockRoute.js

--- routes/stockRoute.js
+++ routes/stockRoute.js
@@ -6,8 +6,9 @@ import {
   updateStockStatus,
   stockSummary,
   addStockIn,
-  getStockItemsByCategory,getDistrictInventory,getDistrictStockItemsByCategory
+  getStockItemsByCategory,getDistrictInventory,getDistrictStockItemsByCategory,uploadStockInItems,getItemQR
 } from "../controller/stock.controller.js";
+import { uploadInventoryFile } from "../middlewares/uploadchallan.js";
 import { auth } from "../middlewares/authMiddleware.js"
 
 const router = express.Router();
@@ -23,7 +24,28 @@ router.post("/stock-in", auth, addStockIn);
 
 //this is for finding the by category according data 
 router.get("/category/:category", auth, getStockItemsByCategory);
-
+router.post(
+  "/inventory/stock-in/upload",
+  auth,
+  uploadInventoryFile.single("file"),
+  uploadStockInItems
+);
+router.get("/items/:itemId/qr", auth, getItemQR);
 
 router.get("/district/inventory/category/:category",auth, getDistrictStockItemsByCategory);
+
+
+
+
+
+
+
+//head stock manage
+
+import {getOverallInventoryDashboard,getOverallCategoryItems } from "../controller/headoffice/headInventoryController.js";
+
+
+
+router.get("/inventory/dashboard", getOverallInventoryDashboard);
+router.get("/inventory/overall/category", getOverallCategoryItems);
 export default router;

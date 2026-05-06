9b2260bbd48603336b5e60b6d87c8ee065cab802
merxentaerp-tech <merxenta.erp@gmail.com>
2026-05-06 15:17:50 +0530

    Update request.js

--- routes/request.js
+++ routes/request.js
@@ -12,10 +12,10 @@ import {
   receiveTransfer,
   getIncomingTransfers,
   getOutgoingTransfers,
-  getAvailableStockForRequest,getTransferDetails,getEWayBillByTransferId,estimateDispatchRequestValue,createDistrictStockRequest
+  getAvailableStockForRequest,getTransferDetails,getEWayBillByTransferId,estimateDispatchRequestValue,createDistrictStockRequest,getHeadStore,getRetailStoresUnderDistrict,approveAndDispatchRequestfromretail
 } from "../controller/stockRequest.controller.js";
 
-import {getHeadReceivedStockRequests,approveAndDispatchHeadRequest,createHeadStockRequest,getHeadAllTransfers} from "../controller/headoffice/headrequestflow.js"
+import {getHeadReceivedStockRequests,approveAndDispatchHeadRequest,createHeadStockRequest,getHeadAllTransfers,getAvailableStoresForHeadRequest,getAnyTransferDetailsForHead} from "../controller/headoffice/headrequestflow.js"
 
 const router = express.Router();
 
@@ -28,6 +28,25 @@ router.get("/requests/:requestId", auth, getStockRequestById);
 router.put("/requests/:requestId/cancel", auth, cancelStockRequest);
 router.put("/requests/:requestId/reject", auth, rejectStockRequest);
 
+
+
+
+
+router.post(
+  "/requests/:requestId/approve-dispatch-from-retail",
+  auth,
+  upload.fields([
+    { name: "driver_photo", maxCount: 1 },
+    { name: "dispatch_images", maxCount: 3 },
+    { name: "dispatch_video", maxCount: 1 },
+    { name: "e_way_bill", maxCount: 1 },
+  ]),
+  approveAndDispatchRequestfromretail
+);
+
+
+
+//this is district 
 router.put(
   "/requests/:requestId/approve-dispatch",
   auth,
@@ -45,6 +64,8 @@ router.get("/transfers/outgoing", auth, getOutgoingTransfers);
 router.put("/transfers/:transferId/receive", auth, receiveTransfer);
 router.get("/transfers/:id/details",auth,getTransferDetails
 );
+
+
 router.get(
   "/transfers/:id/e-way-bill",
   auth,
@@ -55,18 +76,37 @@ router.post(
   auth,
   estimateDispatchRequestValue
 );
+
+
+
+
+// ye district ka request api h to head and to retail
 router.post(
   "/district-stock-request",
   auth,
   createDistrictStockRequest
 );
+router.post(
+  "/head/available-stores",
+  auth,
+  getHeadStore
+)
+router.post(
+  "/head/available-stores",
+  auth,
+  getRetailStoresUnderDistrict
+)
 
 
 
-//head request flow
+
+
+
+
+//head request flow 
 router.get('/headrece',auth,getHeadReceivedStockRequests)
 
-router.put(
+router.post(
   "/requestshead/:requestId/approve-dispatch",
   auth,
   upload.fields([
@@ -77,6 +117,24 @@ router.put(
   approveAndDispatchHeadRequest
 );
 
-router.post("/head-stock-request", auth, createHeadStockRequest);
+router.post(
+  "/head/available-stores",
+  auth,
+  getAvailableStoresForHeadRequest
+);
+
+router.post(
+  "/head/create",
+  auth,
+  createHeadStockRequest
+);
+
+
+router.get(
+  "/head/transfers/:id",
+  auth,
+  getAnyTransferDetailsForHead
+);
+
 router.get("/transfers/head/all", auth, getHeadAllTransfers);
 export default router;

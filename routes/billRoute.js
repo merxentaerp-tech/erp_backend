6fbfd1de6b5b9da517f8b317306a1bcda29e366d
merxentaerp-tech <merxenta.erp@gmail.com>
2026-05-12 17:53:49 +0530

    Update billRoute.js

--- routes/billRoute.js
+++ routes/billRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createInvoiceFromBill,createBill ,scanBillingItem} from "../controller/billingController.js";
+import { createInvoiceFromBill,createBill ,scanBillingItem,createManualBillingEntry} from "../controller/billingController.js";
 import { auth } from "../middlewares/authMiddleware.js"; // apna auth middleware path lagao
 const router = express.Router();
 router.post("/create-bill",auth, createBill);
 router.get("/billing/scan-item/:code", auth, scanBillingItem);
+router.post("/manual-entry", auth, createManualBillingEntry);
 export default router;

ae1748bf24847a42e6a0f80e6f42bfd46fa98310
merxentaerp-tech <merxenta.erp@gmail.com>
2026-05-21 12:14:43 +0530

    Update stock.controller.js

--- controller/stock.controller.js
+++ controller/stock.controller.js
@@ -1,5 +1,5 @@
 import { Op, QueryTypes } from "sequelize";
-import Ledger from "../model/ledger.js";
+import Ledger from "../model/ladger.js";
 import Task from "../model/task.js";
 import sequelize from "../config/db.js";
 import QRCode from "qrcode";

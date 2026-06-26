a134dfe28feddc44bcd9116aa070627fa91d4c73
merxentaerp-tech <merxenta.erp@gmail.com>
2026-06-26 17:02:35 +0530

    Update itemtracker.js

--- routes/itemtracker.js
+++ routes/itemtracker.js
@@ -1,51 +1,39 @@
-// routes/tracker.routes.js
-
 import express from "express";
-import {auth} from "../middlewares/authMiddleware.js";
+import { auth } from "../middlewares/authMiddleware.js";
 
 import {
   getTrackerItems,
   getItemTrackerBatches,
   distributeBatch,
   getBatchFinalDestinations,
-  getBatchNodeRoute,searchBatchTracker,getAllTrackerBatches,getBatchMovementHistory,getBatchMovementHistoryByBatchNo,getItemFinalDestinations,getItemBatchesByDate
+  getBatchNodeRoute,
+  searchBatchTracker,
+  getAllTrackerBatches,
+  getBatchMovementHistory,
+  getBatchMovementHistoryByBatchNo,
+  getItemFinalDestinations,
 } from "../controller/headoffice/itemtrackker.js";
 
 const router = express.Router();
 
-// router.use(authMiddleware);
+router.get("/items", auth, getTrackerItems);
 
-// item list/search
-router.get("/items", auth,getTrackerItems);
+router.get("/items/:item_id/batches", auth, getItemTrackerBatches);
 
-// item ke root batches
-router.get("/items/:item_id/batches", auth,getItemTrackerBatches);
-router.get(
-  "/items/:item_id/batches-by-date",
-  auth,
-  getItemBatchesByDate
-);
+router.get("/items/:item_id/final-destinations", auth, getItemFinalDestinations);
 
-// manual/test distribution
-router.post("/batches/distribute",auth, distributeBatch);
+router.post("/batches/distribute", auth, distributeBatch);
 
-// batch final current locations  use this - for get 
-router.get("/batches/:batch_id/final-destinations",auth, getBatchFinalDestinations);
+router.get("/batches/search", auth, searchBatchTracker);
 
-// batch route/timeline
-router.get("/batches/:batch_id/route",auth, getBatchNodeRoute);
+router.get("/batches", auth, getAllTrackerBatches);
 
+router.get("/batches/:batch_id/final-destinations", auth, getBatchFinalDestinations);
 
-//this is for finding all batche according to 
-router.get("/batches", getAllTrackerBatches);
-router.get("/batches/search", searchBatchTracker);
+router.get("/batches/:batch_id/route", auth, getBatchNodeRoute);
 
-router.get("/batches/:batch_id/history", getBatchMovementHistory);
-router.get("/history", getBatchMovementHistoryByBatchNo);
+router.get("/batches/:batch_id/history", auth, getBatchMovementHistory);
 
+router.get("/history", auth, getBatchMovementHistoryByBatchNo);
 
-
-
-
-router.get("/items/:item_id/final-destinations", getItemFinalDestinations);
 export default router;

907b0fdcbcb51e4e72aff7a795cee1a30d201b78
merxentaerp-tech <merxenta.erp@gmail.com>
2026-06-29 11:42:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ const corsOptions = {
     "http://localhost:3000",
     "http://localhost:5173",
     "https://erp-dash-board.vercel.app",
+    "https://erp-dash-board-stagging-iep5.vercel.app",
   ],
 
   methods: [

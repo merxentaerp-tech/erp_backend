be4186b22f7a4a87e177a2388aa555313ef07225
merxentaerp-tech <merxenta.erp@gmail.com>
2026-07-23 15:36:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 import { Server } from "socket.io";
-// import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 import completeAuditRoutes from "./routes/completeAuditRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -273,7 +274,7 @@ app.get("/sentry-test", (req, res) => {
   ==========================================
 */
 
-// Sentry.setupExpressErrorHandler(app);
+Sentry.setupExpressErrorHandler(app);
 
 /*
   ==========================================
